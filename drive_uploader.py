9d7f6f3ebb215405ff401a1eb5ae81ab252fa580
Sina <sina.ahmari65@email.com>
2025-04-12 13:53:52 -0400

    Add complete Google Drive helper functions

--- drive_uploader.py
+++ drive_uploader.py
@@ -1,54 +1,69 @@
 import os
 import pickle
-import io
-import mimetypes
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseUpload
+from googleapiclient.http import MediaFileUpload
 
 SCOPES = ["https://www.googleapis.com/auth/drive.file"]
+CREDENTIALS_FILE = "client_secrets.json"
+TOKEN_FILE = "token.pickle"
 
-def authenticate():
+def get_drive_service():
     creds = None
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
+    if os.path.exists(TOKEN_FILE):
+        with open(TOKEN_FILE, "rb") as token:
             creds = pickle.load(token)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("client_secrets.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
             creds = flow.run_local_server(port=0)
-        with open("token.pickle", "wb") as token:
+        with open(TOKEN_FILE, "wb") as token:
             pickle.dump(creds, token)
-    return creds
+    service = build("drive", "v3", credentials=creds)
+    return service
 
-def get_or_create_folder(service, parent_id, name):
-    query = f"'{parent_id}' in parents and name='{name}' and mimeType='application/vnd.google-apps.folder' and trashed=false"
-    results = service.files().list(q=query, spaces='drive').execute()
-    files = results.get('files', [])
-    if files:
-        return files[0]['id']
-    file_metadata = {
-        "name": name,
-        "mimeType": "application/vnd.google-apps.folder",
-        "parents": [parent_id]
+def find_or_create_folder(folder_name, parent_folder_id=None):
+    service = get_drive_service()
+    query = f"name = '{folder_name}' and mimeType = 'application/vnd.google-apps.folder'"
+    if parent_folder_id:
+        query += f" and '{parent_folder_id}' in parents"
+    results = service.files().list(q=query, fields="files(id, name)").execute()
+    folders = results.get("files", [])
+    if folders:
+        return folders[0]["id"]
+    metadata = {
+        "name": folder_name,
+        "mimeType": "application/vnd.google-apps.folder"
     }
-    file = service.files().create(body=file_metadata, fields="id").execute()
-    return file.get("id")
+    if parent_folder_id:
+        metadata["parents"] = [parent_folder_id]
+    folder = service.files().create(body=metadata, fields="id").execute()
+    return folder["id"]
+
+def upload_file_to_folder(file_path, parent_folder_name):
+    service = get_drive_service()
+    parent_id = find_or_create_folder(parent_folder_name)
+    file_name = os.path.basename(file_path)
+    file_metadata = {"name": file_name, "parents": [parent_id]}
+    media = MediaFileUpload(file_path)
+    results = service.files().list(q=f"name='{file_name}' and '{parent_id}' in parents", fields="files(id)").execute()
+    existing_files = results.get("files", [])
+    if existing_files:
+        file_id = existing_files[0]["id"]
+        service.files().update(fileId=file_id, media_body=media).execute()
+    else:
+        service.files().create(body=file_metadata, media_body=media, fields="id").execute()
 
 def upload_receipt(file_path, year, category):
-    creds = authenticate()
-    service = build("drive", "v3", credentials=creds)
-
-    base_folder_id = get_or_create_folder(service, "root", "Receipts")
-    year_folder_id = get_or_create_folder(service, base_folder_id, str(year))
-    category_folder_id = get_or_create_folder(service, year_folder_id, category)
-
+    service = get_drive_service()
+    root_id = find_or_create_folder("Receipts")
+    year_id = find_or_create_folder(str(year), root_id)
+    category_id = find_or_create_folder(category, year_id)
     file_name = os.path.basename(file_path)
-    mime_type = mimetypes.guess_type(file_path)[0] or "image/png"
-    file_metadata = {"name": file_name, "parents": [category_folder_id]}
-    media = MediaIoBaseUpload(io.FileIO(file_path, "rb"), mimetype=mime_type)
-    file = service.files().create(body=file_metadata, media_body=media, fields="id").execute()
-    return file.get("id")
+    file_metadata = {"name": file_name, "parents": [category_id]}
+    media = MediaFileUpload(file_path)
+    service.files().create(body=file_metadata, media_body=media, fields="id").execute()
